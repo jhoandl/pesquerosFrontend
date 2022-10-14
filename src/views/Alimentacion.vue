277f6eff0bc5460f06dbc93785862a034195caa6
fernando45y2 <diegofs@arqs.co>
2022-10-13 22:16:42 -0500

    se crea inputs

--- src/views/Alimentacion.vue
+++ src/views/Alimentacion.vue
@@ -1,13 +1,34 @@
 <template>
-    <div class="alimentacion">
-        <p>Alimentación</p>
-    </div>
+  <div class="alimentacion">
+    <b-container fluid style="margin-top: 300px">
+      <b-row class="my-1">
+        <b-col sm="2"> </b-col>
+        <b-col sm="10">
+          <b-form-input
+            id="input-small"
+            size="sm"
+            placeholder="Ingresa la hora de alimentacion"
+          ></b-form-input>
+        </b-col>
+      </b-row>
+
+      <b-row class="my-1" style="margin-top: 600px">
+        <b-col sm="2"> </b-col>
+        <b-col sm="10">
+          <b-form-input
+            id="input-small"
+            placeholder="Ingresa la cantidad"
+          ></b-form-input>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <style lang="scss">
-    .alimentacion {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+.alimentacion {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
