8b78ccf77d9db0bafdaf3612d86f4e8e06ccea4a
diegofs@arqs.co <diegofs@arqs.co>
2022-11-28 21:07:51 -0500

    se sube pantalla de roles en el aplicativo

---
+++ src/views/Roles.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="roles">
+        <p>Roles</p>
+    </div>
+</template>
+
+<style lang="scss">
+    .roles {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
